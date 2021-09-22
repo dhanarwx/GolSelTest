40533ab2b75fe454366495c9c14bc9aac9ad1f1d
b <l>
2021-09-22 22:15:00 +0530

    test cases completed

---
+++ src/test/java/testNG/tc1.java
@@ -0,0 +1,36 @@
+package testNG;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class tc1 {
+  	  
+	  WebDriver driver;
+	   
+		@BeforeMethod
+		public void launch() {
+		  System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+	         /* ChromeOptions Options = new ChromeOptions();
+		  Options.addArguments("headless");
+		  driver = new ChromeDriver(Options); */
+		  driver = new ChromeDriver();  //comment this to run in headless mode and uncomment the above
+		  driver.manage().window().maximize();
+		  driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		  driver.get("http://15.206.186.173:32768/gameoflife/");
+		
+		}
+		
+	  @Test
+	  public void verfiy() {
+		  driver.findElement(By.linkText("New Game")).click();
+  }
+	  @AfterMethod
+	  public void close() {
+		 driver.close();
+	  }
+}
